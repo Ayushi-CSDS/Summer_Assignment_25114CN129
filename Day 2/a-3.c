d1d6b08727655be2b5ae3f2f400c67230f78fb82
Ayushi Yadav <ayushiyadav261207@gmail.com>
2026-06-01 22:33:03 +0530

    Added Day 1 and Day 2

---
+++ Day 2/a-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, r, product = 1;
+
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        r = n % 10;
+        product = product * r;
+        n = n / 10;
+    }
+
+    printf("Product = %d", product);
+
+    return 0;
+}
---
+++ Day 2/a-4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, temp, r, rev = 0;
+
+    scanf("%d", &n);
+
+    temp = n;
+
+    while (n != 0)
+    {
+        r = n % 10;
+        rev = rev * 10 + r;
+        n = n / 10;
+    }
+
+    if (temp == rev)
+        printf("Palindrome Number");
+    else
+        printf("Not a Palindrome Number");
+
+    return 0;
+}
